f1920affb1b11b01d4e24b1e8dedb8537f4155a4
Azizbek <aziz.abdusalomov555@mail.ru>
2025-10-31 10:41:38 +0500

    changed db

--- database/insert_query.py
+++ database/insert_query.py
@@ -6,16 +6,16 @@ logger = logging.getLogger(__name__)
 def insert_user(fullname, phone, gender, address, chat_id):
     try:
         with get_connect() as db:
-            with db.cursor() as dbc:
-                sql = """
-                INSERT INTO users (fullname, phone, gender, address, chat_id) 
-                VALUES (%s, %s, %s, %s, %s)
-                """
-                dbc.execute(sql, (fullname, phone, gender, address, chat_id))
-                db.commit()
-                logger.info(f"User {chat_id} saved to database")
-                return True
-                
+            dbc = db.cursor()
+            sql = """
+            INSERT INTO users (fullname, phone, gender, address, chat_id)
+            VALUES (?, ?, ?, ?, ?)
+            """
+            dbc.execute(sql, (fullname, phone, gender, address, chat_id))
+            db.commit()
+            logger.info(f"User {chat_id} saved to database")
+            return True
+
     except Exception as err:
         logger.error(f"User save error for {chat_id}: {err}")
         return False
--- database/select_query.py
+++ database/select_query.py
@@ -1,38 +1,36 @@
 from .connect import get_connect
 
 
-
 def is_register_by_id(chat_id):
     try:
         with get_connect() as db:
-            with db.cursor() as dbc:
-                dbc.execute("select * from users where chat_id = %s",(chat_id,))
-                return dbc.fetchone()
+            dbc = db.cursor()
+            dbc.execute("SELECT * FROM users WHERE chat_id = ?", (chat_id,))
+            return dbc.fetchone()
     except Exception as err:
         print(f"Register: {err}")
         return None
-    
+
 
 def is_admin_by_id(chat_id):
     try:
         with get_connect() as db:
-            with db.cursor() as dbc:
-                dbc.execute("select * from users where chat_id = %s and is_admin=true",(chat_id,))
-                return dbc.fetchone()
+            dbc = db.cursor()
+            dbc.execute("SELECT * FROM users WHERE chat_id = ? AND is_admin = 1", (chat_id,))
+            return dbc.fetchone()
     except Exception as err:
         print(f"Register: {err}")
         return None
-    
+
 
 def get_category():
     try:
         with get_connect() as db:
             dbc = db.cursor()
-            dbc.execute("SELECT id, name FROM category WHERE is_active = true")
+            dbc.execute("SELECT id, name FROM category WHERE is_active = 1")
             categories = dbc.fetchall()
             dbc.close()
             return categories
     except Exception as err:
         print(f"Get Category error: {err}")
         return None
-
