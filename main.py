7724e8665f2bdab1edd7ccd9060b6ba919d97b94
Azizbek <aziz.abdusalomov555@mail.ru>
2025-10-31 10:51:55 +0500

    changed env

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
-from aiogram import Bot, Dispatcher, F
-from environs import Env
-import logging
-
+from aiogram import Bot, Dispatcher
 import asyncio
+import logging
 
 from handlers import start_router, user_router, admin_router
 
-
-env = Env()
-env.read_env()
+TOKEN = "8369051789:AAFxh26NyE3dAKhJxEsI41-RZM9g-WUERNs"
 
 dp = Dispatcher()
-TOKEN = env.str("TOKEN")
-
 
 async def main():
-    bot = Bot(TOKEN)
+    bot = Bot(token=TOKEN)
     
     dp.include_router(start_router)
     dp.include_router(user_router)
     dp.include_router(admin_router)
 
+    logging.info("Бот запущен...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
-
